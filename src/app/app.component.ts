520f39b9c79dee915d768b2025abbb2f92d7a849
Alain Sarti <alain.sarti@gmail.com>
2017-01-05 20:21:59 +0100

    added i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,54 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
-
-import { DamageTracker } from '../pages/damage_tracker/damage_tracker';
-import { InitiativeTracker } from '../pages/initiative_tracker/initiative_tracker';
-
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
+import {StatusBar, Splashscreen} from 'ionic-native';
+import {DamageTracker} from '../pages/damage_tracker/damage_tracker';
+import {InitiativeTracker} from '../pages/initiative_tracker/initiative_tracker';
+import {TranslateService, TranslatePipe} from 'ng2-translate';
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+    @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = DamageTracker;
+    rootPage: any = DamageTracker;
 
-  pages: Array<{title: string, component: any}>;
+    pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform) {
-    this.initializeApp();
+    constructor(public platform: Platform, public translate: TranslateService) {
+        this.initializeApp();
+        this.translateConfig();
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Damage Tracker', component: DamageTracker },
-      { title: 'Initiative Tracker', component: InitiativeTracker }
-    ];
+        // used for an example of ngFor and navigation
+        this.pages = [
+            {title: 'Damage Tracker', component: DamageTracker},
+            {title: 'Initiative Tracker', component: InitiativeTracker}
+        ];
+    }
 
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+            Splashscreen.hide();
+        });
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.setRoot(page.component);
+    }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+    translateConfig() {
+        var userLang = navigator.language.split('-')[0]; // use navigator lang if available
+        userLang = /(de|en)/gi.test(userLang) ? userLang : 'en';
+
+        // this language will be used as a fallback when a translation isn't found in the current language
+        this.translate.setDefaultLang('en');
+
+        // the lang to use, if the lang isn't available, it will use the current loader to get them
+        this.translate.use(userLang);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { DamageTracker } from '../pages/damage_tracker/damage_tracker';
-import { InitiativeTracker } from '../pages/initiative_tracker/initiative_tracker';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {DamageTracker} from '../pages/damage_tracker/damage_tracker';
+import {InitiativeTracker} from '../pages/initiative_tracker/initiative_tracker';
+import { TranslateModule, TranslateStaticLoader, TranslateLoader } from 'ng2-translate/ng2-translate';
+import { Http } from '@angular/http'
+
+export function createTranslateLoader(http: Http) {
+    return new TranslateStaticLoader(http, 'assets/i18n', '.json');
+}
 
 @NgModule({
-  declarations: [
-    MyApp,
-    DamageTracker,
-    InitiativeTracker
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    DamageTracker,
-    InitiativeTracker
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+    declarations: [
+        MyApp,
+        DamageTracker,
+        InitiativeTracker
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp),
+        TranslateModule.forRoot({
+            provide: TranslateLoader,
+            useFactory: (createTranslateLoader),
+            deps: [Http]
+        })
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        DamageTracker,
+        InitiativeTracker
+    ],
+    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
-export class AppModule {}
+export class AppModule {
+}
